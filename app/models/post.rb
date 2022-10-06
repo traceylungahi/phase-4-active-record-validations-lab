90b4e455f3ac3f3847240e7854862870836bc235
TraceyMmboneLungahi <t.lungahi@gmail.com>
2022-10-06 17:37:02 +0300

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,17 @@ class Post < ApplicationRecord
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { within: ["Fiction", "Non-Fiction"]}
     validate :title_is_clickbait
+
+    TITLE_DOES_NOT_CONTAIN = [
+        /Won't Believe/i,
+        /Guess/i,
+        /Top/i,
+        /Guess/i
+    ]
     
 
     def title_is_clickbait
-        unless title = ("Won't Believe" "Secret" "Top [number]" "Guess")
+        if TITLE_DOES_NOT_CONTAIN.none? {|word| word.match title}
             errors.add(:title, "Not enough clickbait")
         end    
     end 
